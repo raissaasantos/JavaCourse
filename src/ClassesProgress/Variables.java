f19490182caf705d81fb80e9651202540edc071c
Raissa Assis Santos <raissasantos55@yahoo.com>
2023-07-30 20:04:03 -0300

    Commit

---
+++ src/ClassesProgress/Variables.java
@@ -0,0 +1,37 @@
+package ClassesProgress;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int x, y;
+        x = 5;
+        y = 2 * x;
+        System.out.println("Example 1");
+        System.out.println(x);
+        System.out.println(y);
+
+        int x1;
+        double x2;
+        x1 = 5;
+        x2 = 2 * x;
+        System.out.println("Example 2");
+        System.out.println(x1);
+        System.out.println(x2);
+
+        double b, B, h, area;
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+        area = (b + B) / 2.0 * h;
+        System.out.println("Example 3");
+        System.out.println(area);
+
+        int a1, b1;
+        double result;
+        a1 = 5;
+        b1 = 2;
+        result = (double) a1 / b1; //casting
+        System.out.println("Example 4");
+        System.out.println(result);
+    }
+}
