cd93d6f8bfaccebdd4122d1665eb3528f77bffc2
Raissa Assis Santos <raissasantos55@yahoo.com>
2023-08-09 20:33:45 -0300

    Commit

--- src/ClassesExercises/ExExercise.java
+++ src/ClassesExercises/ExExercise.java
@@ -1,5 +1,22 @@
 package ClassesExercises;
 
-public class ExExercise {
+import java.util.Scanner;
 
+public class ExExercise {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double width = sc.nextDouble();
+        double length = sc.nextDouble();
+        double squareMeter = sc.nextDouble();
+
+        double area = width * length;
+        double price = area * squareMeter;
+
+        System.out.printf("Area = %.2f%n", area);
+        System.out.printf("Price = %.2f%n", price);
+
+        sc.close();
+    }
 }
