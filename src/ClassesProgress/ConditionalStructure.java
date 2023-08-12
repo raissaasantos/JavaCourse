3a1c6c11aee281c806ebb9d23d6facceec4ced4f
Raissa Assis Santos <raissasantos55@yahoo.com>
2023-08-12 19:09:50 -0300

    Commit

---
+++ src/ClassesProgress/ConditionalStructure.java
@@ -0,0 +1,27 @@
+package ClassesProgress;
+
+import java.util.Scanner;
+
+public class ConditionalStructure {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int time;
+
+        System.out.println("What time is it? ");
+        time = sc.nextInt();
+
+        if (time < 12) {
+            System.out.println("Good morning!");
+        }
+        else {
+            if (time < 18){
+                System.out.println("Good afternoon!");
+            }
+            else {
+                System.out.println("Good night!");
+            }
+        }
+        sc.close();
+    }
+}
