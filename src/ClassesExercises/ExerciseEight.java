2294af1395ef8dd062280ff6e6d2df126a2c6ef3
Raissa Assis Santos <raissasantos55@yahoo.com>
2023-08-13 19:52:52 -0300

    Commit

--- src/ClassesExercises/ExerciseEight.java
+++ src/ClassesExercises/ExerciseEight.java
@@ -2,18 +2,19 @@ package ClassesExercises;
 
 import java.util.Scanner;
 
-public class ExerciseSeven {
+public class ExerciseEight {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
 
-        if (number < 0){
-            System.out.println("NEGATIVE");
+        if (number % 2 == 0){
+            System.out.println("EVEN NUMBER");
         }
-        else {
-            System.out.println("NOT NEGATIVE");
+        else{
+            System.out.println("ODD NUMBER");
         }
+
         sc.close();
     }
 }
