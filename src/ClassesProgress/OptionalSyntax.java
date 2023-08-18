4534afccbf7da90344675bf598e36df1d6873233
Raissa Assis Santos <raissasantos55@yahoo.com>
2023-08-18 19:32:24 -0300

    Commit

---
+++ src/ClassesProgress/OptionalSyntax.java
@@ -0,0 +1,22 @@
+package ClassesProgress;
+
+import java.util.Scanner;
+
+public class OptionalSyntax {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int minutes = sc.nextInt();
+        double bill = 50.0;
+
+        if (minutes > 100) {
+            bill = bill + (minutes - 100) * 2.0;
+        }
+
+        System.out.printf("Bill = R$ %.2f%n", bill);
+
+        sc.close();
+
+    }
+}
